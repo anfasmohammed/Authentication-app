7473c531be49a96ffd4200528c4ff201e88e4c9c
anfasmohammed <163500801+anfasmohammed@users.noreply.github.com>
2024-10-10 16:04:41 +0530

    db-connected

---
+++ readme.md
@@ -0,0 +1,14 @@
+IIFE:Immediately Invoked Function Expression
+
+500 - internel server error
+200 - success
+201 - created
+400 - bad request
+401 - unautharized request
+404 - not fount
+
+100-199 => informational response
+200-299 => seccess response
+300-399 => redirectional message
+400-499 => client side error
+500-599 => Server side error
--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app=express()
+
+// middlewares
+app.use(cors())// allows to interact whit client which is loaded in different domain.
+app.use(express.json())// instructing the app to accept data in the json format.
+app.use(cookieParser()) // it allows the server to access the use cookies
+app.use(morgan("dev")) // logs requestes errors and more to the console
 app.get("/",(req,res)=>{
     res.send("<h1>Hello World</h1>")
 })
